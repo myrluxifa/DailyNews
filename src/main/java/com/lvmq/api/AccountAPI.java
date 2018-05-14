0173c783982d2740acad0e5782b5c65f902a5577
easy <174030256@qq.com>
2018-05-14 12:54:48 +0800

    remove unused import

--- src/main/java/com/lvmq/api/AccountAPI.java
+++ src/main/java/com/lvmq/api/AccountAPI.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
