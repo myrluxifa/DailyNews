1838165cb3f25ccbd003babc0323e14f9dfafcfe
easy <174030256@qq.com>
2018-05-16 10:14:45 +0800

    modify sign return bean

--- src/main/java/com/lvmq/api/SignAPI.java
+++ src/main/java/com/lvmq/api/SignAPI.java
@@ -26,92 +26,133 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
-@Api(tags = {"签到"})
+@Api(tags = { "签到" })
 @RestController
 @RequestMapping("api/sign")
 public class SignAPI extends BaseAPI {
-	
+
 	@Autowired
 	private SignLogRepository signLogRepository;
-	
+
 	@Autowired
 	private UserLoginRepository userLogRepository;
-	
+
 	@Autowired
 	private GoldLogRepository goldLogRepository;
 
 	@ApiOperation(value = "签到", notes = "", httpMethod = "POST")
 	@ApiImplicitParams({
-			@ApiImplicitParam(paramType = "query", name = "userId", value = "用户ID", required = true, dataType = "String")
-			})
+			@ApiImplicitParam(paramType = "query", name = "userId", value = "用户ID", required = true, dataType = "String") })
 	@PostMapping("do")
 	@Transactional
-	public ResponseBean<Object> done(String userId){
+	public ResponseBean<Object> done(String userId) {
 		try {
 			Calendar cal = Calendar.getInstance();
-			
+
 			String today = TimeUtil.format(cal, "yyyyMMdd");
 			cal.add(Calendar.DAY_OF_MONTH, -1);
 			String yesterday = TimeUtil.format(cal, "yyyyMMdd");
-			
-			List<SignLog> logs = signLogRepository.findByCreateDateInAndUserIdOrderByCreateDateDesc(new String[] {today, yesterday}, userId);
-			//是否连续签到
+
+			List<SignLog> logs = signLogRepository
+					.findByCreateDateInAndUserIdOrderByCreateDateDesc(new String[] { today, yesterday }, userId);
+			// 是否连续签到
 			boolean yes = false;
-			//计算连续时间
+			// 计算连续时间
 			int coun = 1;
-			if(null != logs) {
+			if (null != logs) {
 				for (SignLog sl : logs) {
-					if(sl.getCreateDate().equals(today)) {
-						return new ResponseBean<>(Code.FAIL, Code.FAIL, "请明天再来签到吧~");
+					if (sl.getCreateDate().equals(today)) {
+						return new ResponseBean<>(Code.FAIL, Code.FAIL, new SignRes(sl.getCountinuous(), true, "请明天再来签到吧~"));
 					}
-					if(sl.getCreateDate().equals(yesterday)) {
+					if (sl.getCreateDate().equals(yesterday)) {
 						yes = true;
-						if(sl.getCountinuous() + 1 > 7) {
+						if (sl.getCountinuous() + 1 > 7) {
 							coun = 7;
-						}else {
+						} else {
 							coun = sl.getCountinuous() + 1;
 						}
 					}
 				}
 			}
-			
-			//保存签到信息
+
+			// 保存签到信息
 			SignLog s = new SignLog();
-			//昨天是否签到
-			if(yes) {
+			// 昨天是否签到
+			if (yes) {
 				s.setCountinuous(coun);
-			}else {
+			} else {
 				s.setCountinuous(1);
 			}
-			//金额增加日志
+			// 金额增加日志
 			GoldLog gl = new GoldLog();
-			
-			//增加用户金币
+
+			// 增加用户金币
 			Optional<UserLogin> ul = userLogRepository.findById(userId);
 			UserLogin user = ul.get();
 			gl.setOldNum(user.getGold());
-			
-			user.setGold(user.getGold() + Integer.valueOf(Consts.Sign.COIN.split(",")[coun-1]));
+
+			user.setGold(user.getGold() + Integer.valueOf(Consts.Sign.COIN.split(",")[coun - 1]));
 			user = userLogRepository.save(user);
 			gl.setNewNum(user.getGold());
-			
-			//保存签到信息
+
+			// 保存签到信息
 			s.setCreateDate(today);
 			s.setUserId(userId);
 			signLogRepository.save(s);
-			
-			//写入金币流水
+
+			// 写入金币流水
 			gl.setCreateTime(Calendar.getInstance().getTime());
 			gl.setCreateUser(userId);
-			gl.setNum(Long.valueOf(Consts.Sign.COIN.split(",")[coun-1]));
+			gl.setNum(Long.valueOf(Consts.Sign.COIN.split(",")[coun - 1]));
 			gl.setType(Consts.GoldLog.Type.SIGN);
 			gl.setUserId(userId);
 			goldLogRepository.save(gl);
-			
-			return new ResponseBean<>(Code.SUCCESS, Code.SUCCESS_CODE, "恭喜您，签到成功！");
+
+			return new ResponseBean<>(Code.SUCCESS, Code.SUCCESS_CODE, new SignRes(s.getCountinuous(), true, "恭喜您，签到成功！"));
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			return new ResponseBean<>(Code.FAIL, Code.FAIL, e.getMessage());
 		}
 	}
+}
+
+class SignRes {
+	// 连续签到天数
+	private int countinuous;
+	// 今日是否已签
+	private boolean signed;
+	// 信息
+	private String message;
+
+	public SignRes(int countinuous, boolean signed, String message) {
+		super();
+		this.countinuous = countinuous;
+		this.signed = signed;
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getCountinuous() {
+		return countinuous;
+	}
+
+	public void setCountinuous(int countinuous) {
+		this.countinuous = countinuous;
+	}
+
+	public boolean isSigned() {
+		return signed;
+	}
+
+	public void setSigned(boolean signed) {
+		this.signed = signed;
+	}
+
 }
